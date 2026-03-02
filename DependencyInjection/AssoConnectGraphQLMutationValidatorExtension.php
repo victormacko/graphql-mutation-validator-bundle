a76a1efdb44cdfd39a9238802374868260eca7e4
victor <victor@victor.com.au>
2026-03-02 14:03:32 +1100

    Updated return type

--- DependencyInjection/AssoConnectGraphQLMutationValidatorExtension.php
+++ DependencyInjection/AssoConnectGraphQLMutationValidatorExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 Class AssoConnectGraphQLMutationValidatorExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader(
             $container,
